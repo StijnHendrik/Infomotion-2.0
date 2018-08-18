e31b15c0822f498fc5374c100bb384d52118e38a
Stijn Heynderickx <sheynderickx@inuits.eu>
2018-08-19 00:22:31 +0200

    working on contact page + more styling on post pages

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,4 +8,8 @@ use App\Http\Controllers\Controller;
 class ContactController extends Controller
 {
     //
+    public function contact()
+    {
+        return view('contact.contact');
+    }
 }
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,6 +14,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('home') }}">Home</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+                </li>
                 @guest
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
