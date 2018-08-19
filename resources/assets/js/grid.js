b7350c9b2021b8698d05c4785ba63da8ad8e5f65
Hendrik Van Dijck <hendrik@pau.be>
2018-08-19 12:17:33 +0200

    fix media slideshow grid

--- resources/assets/js/grid.js
+++ resources/assets/js/grid.js
@@ -5,22 +5,22 @@ window.onload = function () {
     setInterval(function () {slideshow ()},500)
 }
 
-window.slideshow = function () {
-    var item__media = document.getElementsByClassName('grid-item__media')
-
-    let media = item__media[slideIndex].getElementsByClassName('slide')
-    for (let j = 0; j < media.length; j++) {
-        console.log(media[j].dataset.test)
-        media[j].style.display = "none"
-    }
-
-    if ((counter >= media.length)) {
-        counter = 0
-    }
-        media[counter].style.display = "block"
-    counter++
-    slideIndex++
-    if (slideIndex > item__media.length) {
-        slideIndex = 0
-    }
-}
+// window.slideshow = function () {
+//     var item__media = document.getElementsByClassName('grid-item__media')
+//
+//     let media = item__media[slideIndex].getElementsByClassName('slide')
+//     for (let j = 0; j < media.length; j++) {
+//         console.log(media[j].dataset.test)
+//         media[j].style.display = "none"
+//     }
+//
+//     if ((counter >= media.length)) {
+//         counter = 0
+//     }
+//         media[counter].style.display = "block"
+//     counter++
+//     slideIndex++
+//     if (slideIndex > item__media.length) {
+//         slideIndex = 0
+//     }
+// }
